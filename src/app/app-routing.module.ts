18029a6cff0626ff6831b1c23e1f91ef06bd2b85
Albert Sigsbert <ishengomaalbert@gmail.com>
2023-06-14 11:13:49 +0300

    Protecting routes with canActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 
+//Component
 import { HomeComponent } from "./home/home.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
@@ -9,6 +10,7 @@ import { ServerComponent } from "./servers/server/server.component";
 import { ServersComponent } from "./servers/servers.component";
 import { UserComponent } from "./users/user/user.component";
 import { UsersComponent } from "./users/users.component";
+import { AuthGuard } from "./auth-guard.service";
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
@@ -20,6 +22,7 @@ const appRoutes: Routes = [
   
     {
       path: 'servers',
+      canActivate:[AuthGuard],
       component: ServersComponent,
       children: [
         { path: ':id', component: ServerComponent },
